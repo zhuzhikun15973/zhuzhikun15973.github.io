137c56b24544d538eba6f165d7ed5ce703cd27ad
zhikunzhu <zz1u17@soton.ac.uk>
2018-07-05 16:09:16 +0100

    bug fix

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
             var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
             var height = Math.max(bHeight, dHeight);
             iframe.height = height;
-            console.log(height);
+            
             }catch (ex){}
             }
             window.setInterval("reinitIframe()", 200);
